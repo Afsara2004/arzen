aa32ab17e9268bbcf22af6fe5bee9fdc308a9bde
Afsara2004 <148635231+Afsara2004@users.noreply.github.com>
2024-06-03 21:19:38 +0530

    Update collection.js

--- collection.js
+++ collection.js
@@ -1,18 +1,18 @@
-var search=document.getElementById("search");
-var product=document.getElementById("product");
-var box=product.querySelectorAll(".box");
+// var search=document.getElementById("search");
+// var product=document.getElementById("product");
+// var box=product.querySelectorAll(".box");
 
-search.addEventListener("keyup",function(){
-    var enter=event.target.value.toUppercase();
+// search.addEventListener("keyup",function(){
+//     var enter=event.target.value.toUppercase();
 
-    for(var count=0;count<=box.length;count++){
-        var name=box[count].querySelector("p").textContent;
+//     for(var count=0;count<=box.length;count++){
+//         var name=box[count].querySelector("p").textContent;
 
-        if(name.toUppercase().indexOf(enter)<0){
-            box[count].style.display="none";
-        }
-        else{
-            box[count].style.display="block";
-        }
-    }
-});
+//         if(name.toUppercase().indexOf(enter)<0){
+//             box[count].style.display="none";
+//         }
+//         else{
+//             box[count].style.display="block";
+//         }
+//     }
+// });
