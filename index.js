32039bfb5cb8ead45a8ea2a02a95b982a891e699
Afsara2004 <148635231+Afsara2004@users.noreply.github.com>
2024-06-03 20:25:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function showNav(){
+    var side=document.querySelector(".side-navbar");
+
+side.style.display= "block";
+}
+function closeNav(){
+    var side=document.querySelector(".side-navbar");
+
+    side.style.display= "none";
+    }
